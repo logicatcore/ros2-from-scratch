3af52c30f866cabe178ce958bf21ca1a53b96ecb
Sai Sharath Kakubal <kakubalsai@gmail.com>
2026-07-12 13:42:37 +0000

    Add timer based callback

--- module4_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
+++ module4_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -6,7 +6,12 @@ class DemoNode(Node):
     def __init__(self):
         super().__init__("DemoNode")
         self.get_logger().info("DemoNode has been started")
-
+        self.counter_ = 0
+        self.timer_ = self.create_timer(1.0, self.timer_callback) 
+        
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info(f"Timer called {self.counter_} times")
 
 def main(args=None):
     rclpy.init(args=args)
